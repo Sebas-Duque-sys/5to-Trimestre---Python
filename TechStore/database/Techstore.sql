a6e44cf99a9d599ee57fa8637a1b96bb1192b359
Sebas-Duque-sys <juansebastianduquegomez@gmail.com>
2026-09-04 09:36:50 -0500

    -SQL
    -Archivo para la creación de la Base de Datos 'Techstore' y las tablas 'Usuarios' y 'Productos'.

---
+++ TechStore/database/Techstore.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE techstore;
+
+USE techstore;
+CREATE TABLE usuarios(
+id INT auto_increment PRIMARY KEY,
+nombre VARCHAR(80) NOT NULL,
+correo VARCHAR(100) UNIQUE NOT NULL,
+telefono VARCHAR(20) NOT NULL,
+password VARCHAR(255) NOT NULL,
+rol ENUM('Administrador', 'Cliente') DEFAULT 'Cliente',
+estado ENUM('Activo','Inactivo') DEFAULT 'Activo');
+
+INSERT INTO usuarios(nombre, correo, telefono, password, rol, estado) VALUES
+('Sebastian', 'jsdg@gmail.com', '3217252560', 'ADSO321', 'Administrador', 'Activo'),
+('Admin', 'admin@techsore.com', '3213214321', 'SENA60', 'Administrador', 'Activo');
+
+CREATE TABLE productos(
+codigo VARCHAR(20) PRIMARY KEY,
+nombre VARCHAR(80) NOT NULL,
+precio DECIMAL(10,2) NOT NULL,
+categoria VARCHAR(50)
+);
+
+INSERT INTO productos VALUES
+('P001','Laptop Lenovo',3500000,'Computadores'),
+('P002','Mouse Logitech',85000,'Accesorios'),
+('P003','Monitor Samsung',890000,'Monitores'),
+('P004', 'Portátil Lenovo IdeaPad 3', 2850000, 'Computadores');
