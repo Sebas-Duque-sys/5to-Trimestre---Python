ef764932715e1d1bb85a0e8fb5510e0223d8295f
Sebas-Duque-sys <juansebastianduquegomez@gmail.com>
2026-09-25 14:40:57 -0500

    -Contraseña e Imágenes
    -Permitir la subida y almacenamiento de imágenes

--- TechStore/database/Techstore.sql
+++ TechStore/database/Techstore.sql
@@ -18,11 +18,12 @@ CREATE TABLE productos(
 codigo VARCHAR(20) PRIMARY KEY,
 nombre VARCHAR(80) NOT NULL,
 precio DECIMAL(10,2) NOT NULL,
-categoria VARCHAR(50)
+categoria VARCHAR(50),
+imagen_url VARCHAR(255)
 );
 
 INSERT INTO productos VALUES
-('P001','Laptop Lenovo',3500000,'Computadores'),
-('P002','Mouse Logitech',85000,'Accesorios'),
-('P003','Monitor Samsung',890000,'Monitores'),
-('P004', 'Portátil Lenovo IdeaPad 3', 2850000, 'Computadores');
+('P001','Laptop Lenovo',3500000,'Computadores','w=1160.webp'),
+('P002','Mouse Logitech',85000,'Accesorios','mouse-gamer-logitech-g203-lightsync-rgb-6-botones-8000dpi.webp'),
+('P003','Monitor Samsung',890000,'Monitores','Monitor-Samsung-19-pulgadas-HD-S33A-Vista-Angulo-Izquierdo.webp'),
+('P004', 'Portátil Lenovo IdeaPad 3', 2850000, 'Computadores','portatil-lenovo-ideapad-3-14-core-i5-8gb-512gb-ssd-w10.webp');
